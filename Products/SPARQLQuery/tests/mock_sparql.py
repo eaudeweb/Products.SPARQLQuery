883bb6f7ad0e772c3f749d469847308f31ac0d94
Alex Morega <alex@grep.ro>
2011-05-05 12:55:16 +0300

    factor out SPARQL mocking to a separate module

---
+++ Products/SPARQLQuery/tests/mock_sparql.py
@@ -0,0 +1,63 @@
+import urllib2
+from StringIO import StringIO
+from mock import Mock, patch
+
+SPARQL_GET_LANGS = """\
+SELECT ?lang_url WHERE {
+  ?lang_url a <http://rdfdata.eionet.europa.eu/eea/ontology/Language> .
+}
+"""
+
+SPARQL_RESPONSES = {}
+SPARQL_RESPONSES[SPARQL_GET_LANGS] = """\
+<?xml version='1.0' encoding='UTF-8'?>
+<sparql xmlns='http://www.w3.org/2005/sparql-results#'>
+  <head>
+    <variable name='lang_url'/>
+  </head>
+  <results>
+    <result>
+      <binding name='lang_url'>
+        <uri>http://rdfdata.eionet.europa.eu/eea/languages/en</uri>
+      </binding>
+    </result>
+    <result>
+      <binding name='lang_url'>
+        <uri>http://rdfdata.eionet.europa.eu/eea/languages/de</uri>
+      </binding>
+    </result>
+  </results>
+</sparql>
+"""
+
+def respond_to_sparql(query):
+    for key in SPARQL_RESPONSES:
+        if query == key.replace("\n", " ").encode('utf-8'):
+            return SPARQL_RESPONSES[key]
+    else:
+        raise ValueError("unknown query: %r" % query)
+
+def mock_urlopen(request):
+    try:
+        from urlparse import parse_qs
+    except ImportError:
+        from cgi import parse_qs
+    query = parse_qs(request.get_data())['query'][0]
+
+    response = Mock()
+    response.fp = StringIO(respond_to_sparql(query))
+    return response
+
+class MockSparql(object):
+    queries = {
+        'get_languages': SPARQL_GET_LANGS,
+    }
+
+    def start(self):
+        self.urllib2_patch = patch('sparql.urllib2')
+        mock_urllib2 = self.urllib2_patch.start()
+        mock_urllib2.Request = urllib2.Request
+        mock_urllib2.urlopen = mock_urlopen
+
+    def stop(self):
+        self.urllib2_patch.stop()
--- Products/SPARQLQuery/tests/test_query.py
+++ Products/SPARQLQuery/tests/test_query.py
@@ -1,70 +1,22 @@
-import urllib2
-from StringIO import StringIO
 import unittest
-from mock import Mock, patch
+from mock import patch
+from mock_sparql import MockSparql
 
-SPARQL_GET_LANGS = """\
-SELECT ?lang_url WHERE {
-  ?lang_url a <http://rdfdata.eionet.europa.eu/eea/ontology/Language> .
-}
-"""
-
-SPARQL_RESPONSES = {}
-SPARQL_RESPONSES[SPARQL_GET_LANGS] = """\
-<?xml version='1.0' encoding='UTF-8'?>
-<sparql xmlns='http://www.w3.org/2005/sparql-results#'>
-	<head>
-		<variable name='lang_url'/>
-	</head>
-	<results>
-		<result>
-			<binding name='lang_url'>
-				<uri>http://rdfdata.eionet.europa.eu/eea/languages/en</uri>
-			</binding>
-		</result>
-		<result>
-			<binding name='lang_url'>
-				<uri>http://rdfdata.eionet.europa.eu/eea/languages/de</uri>
-			</binding>
-		</result>
-	</results>
-</sparql>
-"""
-
-def respond_to_sparql(query):
-    for key in SPARQL_RESPONSES:
-        if query == key.replace("\n", " ").encode('utf-8'):
-            return SPARQL_RESPONSES[key]
-    else:
-        raise ValueError("unknown query: %r" % query)
-
-def mock_urlopen(request):
-    try:
-        from urlparse import parse_qs
-    except ImportError:
-        from cgi import parse_qs
-    query = parse_qs(request.get_data())['query'][0]
-
-    response = Mock()
-    response.fp = StringIO(respond_to_sparql(query))
-    return response
 
 class QueryTest(unittest.TestCase):
     def setUp(self):
         from Products.SPARQLQuery.Query import SPARQLQuery
         self.query = SPARQLQuery('sq', "Test Query", "_endpoint_", "_query_")
-        self.urllib2_patch = patch('sparql.urllib2')
-        mock_urllib2 = self.urllib2_patch.start()
-        mock_urllib2.Request = urllib2.Request
-        mock_urllib2.urlopen = mock_urlopen
+        self.mock_sparql = MockSparql()
+        self.mock_sparql.start()
 
     def tearDown(self):
-        self.urllib2_patch.stop()
+        self.mock_sparql.stop()
 
     def test_simple_query(self):
         from sparql import IRI
         self.query.endpoint_url = "http://cr3.eionet.europa.eu/sparql"
-        self.query.query = SPARQL_GET_LANGS
+        self.query.query = self.mock_sparql.queries['get_languages']
         result = self.query.execute()
         self.assertEqual(result.fetchall(), [
             (IRI('http://rdfdata.eionet.europa.eu/eea/languages/en'),),
@@ -75,7 +27,7 @@ class QueryTest(unittest.TestCase):
     def test_timeout(self, mock_threading):
         from Products.SPARQLQuery.Query import QueryTimeout
         self.query.endpoint_url = "http://cr3.eionet.europa.eu/sparql"
-        self.query.query = SPARQL_GET_LANGS
+        self.query.query = self.mock_sparql.queries['get_languages']
         mock_threading.Thread.return_value.isAlive.return_value = True
 
         self.assertRaises(QueryTimeout, self.query.execute)
@@ -83,7 +35,7 @@ class QueryTest(unittest.TestCase):
     @patch('Products.SPARQLQuery.Query.sparql')
     def test_error(self, mock_sparql):
         self.query.endpoint_url = "http://cr3.eionet.europa.eu/sparql"
-        self.query.query = SPARQL_GET_LANGS
+        self.query.query = self.mock_sparql.queries['get_languages']
         class MyError(Exception): pass
         mock_sparql.query.side_effect = MyError
 

b2373f2e9096c06947d92a3b4e09952d8b1a6254
Alex Morega <alex@grep.ro>
2011-05-12 15:25:05 +0300

    adapt to change in sparql library

--- Products/SPARQLQuery/tests/test_browser.py
+++ Products/SPARQLQuery/tests/test_browser.py
@@ -61,7 +61,7 @@ class BrowserTest(unittest.TestCase):
                       for tr in css(table, 'tbody tr')]
         self.assertEqual(len(table_data), 45)
         lang_da_url = 'http://rdfdata.eionet.europa.eu/eea/languages/da'
-        self.assertEqual(table_data[7], ['<'+lang_da_url+'>', '"Danish"'])
+        self.assertEqual(table_data[7], ['<'+lang_da_url+'>', "'Danish'@en"])
 
     def test_with_literal_argument(self):
         br = self.browser
